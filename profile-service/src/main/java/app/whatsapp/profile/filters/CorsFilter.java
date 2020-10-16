673bcaa85563c9abd71ee42493e1fb94e2228442
usrivastava92 <usrivastava92@gmail.com>
2020-10-17 01:02:39 +0530

    added GC argument

--- profile-service/src/main/java/app/whatsapp/profile/filters/CorsFilter.java
+++ profile-service/src/main/java/app/whatsapp/profile/filters/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter extends org.springframework.web.filter.CorsFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:3000");
+        response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,HEAD,OPTIONS,POST,PUT");
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Set-Cookie");
