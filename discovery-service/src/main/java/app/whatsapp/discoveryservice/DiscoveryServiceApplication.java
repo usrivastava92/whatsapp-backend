ffdb6b486733643b82b7fd65e7add33e3d1e6aed
usrivastava92 <usrivastava92@gmail.com>
2020-10-19 12:36:10 +0530

    added component scan

--- discovery-service/src/main/java/app/whatsapp/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/app/whatsapp/discoveryservice/DiscoveryServiceApplication.java
@@ -3,11 +3,13 @@ package app.whatsapp.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableEurekaServer
 @SpringBootApplication
+@ComponentScan(basePackages = "app.whatsapp")
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
