ba6c2c52b45a7a6569948198b670292072e2d4d2
usrivastava92 <usrivastava92@gmail.com>
2020-10-19 12:30:07 +0530

    added ui app in heartbeat check

--- discovery-service/src/main/java/app/whatsapp/discoveryservice/schedulers/HeartBeatScheduler.java
+++ discovery-service/src/main/java/app/whatsapp/discoveryservice/schedulers/HeartBeatScheduler.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,17 +20,21 @@ import org.springframework.web.client.RestTemplate;
 Hack scheduler to keep heroku servers alive
  */
 @Component
-@Import(CommonWebSpringConfig.class)
+@Profile("prod")
 public class HeartBeatScheduler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HeartBeatScheduler.class);
 
     @Value("${application.web.protocol}://${HOSTNAME}${eureka.dashboard.path}")
     private String selfUrl;
+    @Value("${application.ui.app.uri}")
+    private String uiApp;
 
-    @Autowired
-    @Qualifier("restTemplate")
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public HeartBeatScheduler(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Scheduled(fixedDelayString = "${applications.heartbeat.millis}")
     public void heartbeat() {
@@ -50,9 +55,15 @@ public class HeartBeatScheduler {
         });
         try {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(selfUrl, String.class);
-            LOGGER.info("Checking heartbeat for : SELF, URI : {}, HttpStatusCode : {}", selfUrl.replace("/eureka", ""), responseEntity.getStatusCode());
+            LOGGER.info("Checking heartbeat for : SELF, URI : {}, HttpStatusCode : {}", selfUrl, responseEntity.getStatusCode());
         } catch (Exception e) {
             LOGGER.error("Error in checking heartbeat for : SELF , {}", e);
+        }
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(uiApp, String.class);
+            LOGGER.info("Checking heartbeat for : SELF, UI-App : {}, HttpStatusCode : {}", uiApp, responseEntity.getStatusCode());
+        } catch (Exception e) {
+            LOGGER.error("Error in checking heartbeat for : UI-App , {}", e);
         }
     }
 
