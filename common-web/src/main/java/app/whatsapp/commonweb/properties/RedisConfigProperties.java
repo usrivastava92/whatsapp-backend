a93bdb15962a2a31b823ffa4aae5b197ffca29d1
usrivastava92 <usrivastava92@gmail.com>
2020-10-23 23:58:55 +0530

    added redis uri config

--- common-web/src/main/java/app/whatsapp/commonweb/properties/RedisConfigProperties.java
+++ common-web/src/main/java/app/whatsapp/commonweb/properties/RedisConfigProperties.java
@@ -1,15 +1,21 @@
 package app.whatsapp.commonweb.properties;
 
+import app.whatsapp.common.constants.CommonConstants;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
+@Slf4j
 @Component
 @ConfigurationProperties(prefix = "application.redis")
 @ConditionalOnProperty(value = "application.redis.enable", havingValue = "true")
 public class RedisConfigProperties {
+
+    private static final String SCHEMA_SEPARATOR = "://";
 
     private boolean enable;
     private String hostname;
@@ -19,6 +25,32 @@ public class RedisConfigProperties {
     private Long defaultTtlSeconds;
     private Long connectTimeoutMs;
     private Long commandTimeoutMs;
+    private String uri;
 
+    public RedisConfigProperties() {
+        log.info("Initializing redis configuration");
+        populateDetailsUsingUri();
+    }
+
+    private void populateDetailsUsingUri() {
+        if (StringUtils.isNotBlank(uri)) {
+            String[] tokens = uri.split(SCHEMA_SEPARATOR);
+            if (tokens.length < 2) {
+                throw new IllegalArgumentException("redis uri is invalid");
+            }
+            String[] creds = tokens[1].split(CommonConstants.SpecialChars.COLON);
+            String user = creds[0];
+            if (creds.length < 3) {
+                throw new IllegalArgumentException("redis uri is invalid");
+            }
+            this.port = Integer.parseInt(creds[2]);
+            String[] passHost = creds[1].split(CommonConstants.SpecialChars.AT_THE_RATE);
+            if (passHost.length < 2) {
+                throw new IllegalArgumentException("redis uri is invalid");
+            }
+            this.password = passHost[0];
+            this.hostname = passHost[1];
+        }
+    }
 
 }
--- gateway-service/src/main/java/app/whatsapp/gateway/config/WebSocketStompConfig.java
+++ gateway-service/src/main/java/app/whatsapp/gateway/config/WebSocketStompConfig.java
@@ -2,14 +2,12 @@ package app.whatsapp.gateway.config;
 
 import app.whatsapp.common.constants.CommonConstants;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Slf4j
