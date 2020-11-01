bb55f3565da99471527428362959842988bb75eb
usrivastava92 <usrivastava92@gmail.com>
2020-11-01 10:11:57 +0530

    added service activation controller for heroku

--- discovery-service/src/main/java/app/whatsapp/discoveryservice/controller/ActivateAllServicesController.java
+++ discovery-service/src/main/java/app/whatsapp/discoveryservice/controller/ActivateAllServicesController.java
@@ -3,6 +3,8 @@ package app.whatsapp.discoveryservice.controller;
 import com.netflix.discovery.shared.Applications;
 import com.netflix.eureka.EurekaServerContextHolder;
 import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 Hack scheduler to keep heroku servers alive
@@ -28,15 +33,19 @@ public class ActivateAllServicesController {
     private String uiAppUrl;
 
     private final RestTemplate restTemplate;
+    private static final String ERROR = "ERROR";
+    private static final String SELF = "SELF";
+    private static final String UI_APP = "UI-App";
 
     public ActivateAllServicesController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping("/activate")
-    public void heartbeat() {
+    public ResponseEntity<ActivateServerResponse> heartbeat() {
         PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
         Applications applications = registry.getApplications();
+        ActivateServerResponse activateServerResponse = new ActivateServerResponse();
         if (applications.getRegisteredApplications().isEmpty()) {
             log.info("No applications registered..!!");
         }
@@ -44,24 +53,52 @@ public class ActivateAllServicesController {
             registeredApplication.getInstances().forEach((instance) -> {
                 try {
                     ResponseEntity<String> responseEntity = restTemplate.getForEntity(instance.getStatusPageUrl(), String.class);
+                    activateServerResponse.addHeartbeat(new Heartbeat(instance.getAppName(), instance.getStatusPageUrl(), responseEntity.getStatusCode().toString()));
                     log.info("Checking heartbeat for : {} : {} : {}", instance.getAppName(), instance.getStatusPageUrl(), responseEntity.getStatusCode());
                 } catch (Exception e) {
                     log.error("Error in checking heartbeat for : {} : {}", instance.getAppName(), instance.getStatusPageUrl());
+                    activateServerResponse.addHeartbeat(new Heartbeat(instance.getAppName(), instance.getStatusPageUrl(), ERROR));
                 }
             });
         });
         try {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(selfUrl, String.class);
+            activateServerResponse.addHeartbeat(new Heartbeat(SELF, selfUrl, responseEntity.getStatusCode().toString()));
             log.info("Checking heartbeat for : SELF : {} : {}", selfUrl, responseEntity.getStatusCode());
         } catch (Exception e) {
+            activateServerResponse.addHeartbeat(new Heartbeat(SELF, selfUrl, ERROR));
             log.error("Error in checking heartbeat for : SELF , {}", selfUrl);
         }
         try {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(uiAppUrl, String.class);
+            activateServerResponse.addHeartbeat(new Heartbeat(UI_APP, uiAppUrl, responseEntity.getStatusCode().toString()));
             log.info("Checking heartbeat for : UI-App : {} : {}", uiAppUrl, responseEntity.getStatusCode());
         } catch (Exception e) {
+            activateServerResponse.addHeartbeat(new Heartbeat(UI_APP, uiAppUrl, ERROR));
             log.error("Error in checking heartbeat for : UI-App , {}", uiAppUrl);
         }
+        return ResponseEntity.ok(activateServerResponse);
     }
 
+
+    private static class ActivateServerResponse {
+        private final List<Heartbeat> heartbeats;
+
+        public ActivateServerResponse() {
+            heartbeats = new ArrayList<>();
+        }
+
+        public void addHeartbeat(Heartbeat heartbeat) {
+            heartbeats.add(heartbeat);
+        }
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Heartbeat {
+        private String name;
+        private String url;
+        private String status;
+    }
 }
